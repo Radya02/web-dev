87c4270a8b9bd317324d9c605ce1eddc6122bfba
Radyyaaaa <jishin02@mail.ru>
2024-12-17 03:13:31 +0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+
+const sideMenu = document.querySelector("aside");
+const profileBtn = document.querySelector("#profile-btn");
+const themeToggler = document.querySelector(".theme-toggler");
+
+profileBtn.onclick = function() {
+    sideMenu.classList.toggle('active');
+}
+window.onscroll = () => {
+    sideMenu.classList.remove('active');
+    if(window.scrollY > 0){document.querySelector('header').classList.add('active');}
+    else{document.querySelector('header').classList.remove('active');}
+}
+
+themeToggler.onclick = function() {
+    document.body.classList.toggle('dark-theme');
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active')
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active')
+}
+
+prevDay.onclick = function() {
+    day>=1 ? day-- : day=6;    
+    setData(day);
+}
+
+setData(day); //To set the data in the table on loading window.
+document.querySelector('.timetable div h2').innerHTML = "Today's Timetable"; //To prevent overwriting the heading on loading;
